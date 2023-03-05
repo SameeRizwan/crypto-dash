a5c72fd5089d6398fc0547502cc55bb1b04f9902
Samee Rizwan <srizwan@banquemisr.com>
2023-03-05 23:43:50 +0500

    Remove node backend

--- Model/option.js
+++
@@ -1,9 +0,0 @@
-const { DataTypes } = require("sequelize");
-
-module.exports = (sequelize) => {
-    const Option = sequelize.define('option', {
-    description: DataTypes.STRING,
-    correctAnswer: DataTypes.BOOLEAN, 
-    })
-    return Option
-}
--- Utils/response.js
+++
@@ -1,6 +0,0 @@
-const RESPONSE_CODE = {
-    200: 200,
-    500: 500
-}
-
-module.exports = RESPONSE_CODE;
